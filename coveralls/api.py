5e85cb3546a8da0a74507a11038b1acef50c134f
z4r <24erre@gmail.com>
2013-03-12 10:29:51 +0100

    utf-8 test

--- coveralls/api.py
+++ coveralls/api.py
@@ -4,6 +4,11 @@ import requests
 
 
 def post(url, repo_token, service_job_id, service_name, git, source_files):
+    json_file = build_file(repo_token, service_job_id, service_name, git, source_files)
+    return requests.post(url, files={'json_file': json_file})
+
+
+def build_file(repo_token, service_job_id, service_name, git, source_files):
     content = {
         'service_job_id': service_job_id,
         'service_name': service_name,
@@ -12,5 +17,4 @@ def post(url, repo_token, service_job_id, service_name, git, source_files):
     }
     if repo_token:
         content['repo_token'] = repo_token
-    json_file = StringIO(json.dumps(content))
-    return requests.post(url, files={'json_file': json_file})
+    return StringIO(json.dumps(content))
--- coveralls/tests.py
+++ coveralls/tests.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+import json
 from unittest import TestCase
 from httpretty import HTTPretty, httprettified
 from coveralls import api, repository, control, wear
@@ -19,9 +21,9 @@ GIT_EXP = {
         'committer_email': '24erre@gmail.com',
         'author_email': '24erre@gmail.com',
         'author_name': u'z4r',
-        'message': u'1st commit with a test message',
+        'message': u'coding utf8 example',
         'committer_name': u'z4r',
-        'id': '17b8119796516195527dcb4f454a2ebd41d60244'
+        'id': '6050a96a400cecba053e457e1e5187d3af0ede1e'
     },
     'remotes': [
         {
@@ -39,10 +41,16 @@ SOURCE_FILES = [
         'coverage': [1]
     },
     {
+        'source': '# coding=utf-8\nEUR = "€"\n\n\ndef amount(tariff, currency=EUR):\n    return \'{0} {1:.2f}\'.format(currency, float(tariff))',
+        'name': 'example/exencode.py',
+        'coverage': [None, 1, None, None, 1, 1]
+    },
+    {
         'source': "def exsum(a, b):\n    # A comment of a exsum\n    return a + b\n\n\ndef exdiff(a, b):\n    return a - b\n\n\nif __name__ == '__main__':\n    print exsum(3,4)\n    print exdiff(2,2)",
         'name': 'example/exmath.py',
         'coverage': [1, None, 1, None, None, 1, 0, None, None, None, None, None]
     }
+
 ]
 
 
@@ -82,3 +90,16 @@ class CoverallsTestCase(TestCase):
             source_files=SOURCE_FILES
         )
         self.assertEqual(response.json(), {u'url': u'https://coveralls.io/jobs/5722', u'message': u'Job #5.1 - 100.0% Covered'})
+
+    def test_build_file_eur(self):
+        json_file = api.build_file(
+            repo_token=Arguments.repo_token,
+            service_job_id=Arguments.service_job_id,
+            service_name=Arguments.service_name,
+            git=GIT_EXP,
+            source_files=SOURCE_FILES
+        )
+        self.assertEqual(
+            json.loads(json_file.read())['source_files'][1]['source'],
+            u'# coding=utf-8\nEUR = "€"\n\n\ndef amount(tariff, currency=EUR):\n    return \'{0} {1:.2f}\'.format(currency, float(tariff))'
+        )
